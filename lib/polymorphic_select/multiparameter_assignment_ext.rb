36337d47d0663553cae90d8eb2d7f370c96cffaa
Geoff Garside <geoff@geoffgarside.co.uk>
2011-10-04 11:29:03 +0100

    Use alias_method_chain style for AR::Base overrides
    Use the ActiveSupport::Concern included block to alias the method we're
    overriding out of the way and put ours in its place. This resolves the
    issue where ActiveRecord::Base was either not accepting the overridding
    method or was itself overriding it.

--- lib/polymorphic_select/multiparameter_assignment_ext.rb
+++ lib/polymorphic_select/multiparameter_assignment_ext.rb
@@ -9,16 +9,22 @@ module PolymorphicSelect
   module MultiparameterAssignmentExt
     extend ActiveSupport::Concern
 
+    included do
+      alias_method :read_value_from_parameter_without_polymorphs, :read_value_from_parameter
+      alias_method :read_value_from_parameter, :read_value_from_parameter_with_polymorphs
+    end
+
     private
-      def read_value_from_parameter(name, values_hash_from_param)
+      def read_value_from_parameter_with_polymorphs(name, values_hash_from_param)
         begin
-          super(name, values_hash_from_param)
+          read_value_from_parameter_without_polymorphs(name, values_hash_from_param)
         rescue NoMethodError => e
           reflection = self.class.reflect_on_association(name.to_sym)
           raise e if reflection.nil? || reflection.options[:polymorphic] != true
           read_polymorphic_parameter_value(name, values_hash_from_param)
         end
       end
+
       def read_polymorphic_parameter_value(name, values_hash_from_param)
         polymorphic_type, polymorphic_id = values_hash_from_param[1].split(':', 2)
         polymorphic_klass = Object.const_get(polymorphic_type) rescue nil
